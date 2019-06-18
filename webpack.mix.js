040f925663b5e5543291537c1256a54ff27cefcd
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-06-18 09:52:01 +0900

    npmで必要なjsライブラリのinstall & vueを使うために設定の追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.browserSync('vuesplash.test')
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
+  .version()
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
