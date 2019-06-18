648936d60301bbfa2cb76d49a3c25178a90abda3
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-06-18 10:04:43 +0900

    npm run watchできるところまで一旦作成

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,3 @@ mix.browserSync('vuesplash.test')
   .js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
   .version()
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
