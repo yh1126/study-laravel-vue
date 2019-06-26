2edf0c1714affbe7b5290c68f24fdae1f76421ee
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-06-26 10:34:56 +0900

    ログイン用テストの作成

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Http\Request;
 */
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
---
+++ tests/Feature/LoginApiTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginApiTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * テストのため、ユーザ作成処理を入れておく
+     */
+    public function setUp() :void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function should_登録済みのユーザーを認証して返却する()
+    {
+        $response = $this->json('POST', route('login'), [
+            'email'    => $this->user->email,
+            'password' => 'password', //参考にしたサイトではsecretになっていたので注意(Laravelのバージョンが違うせい)
+            // setUpで作っているユーザは「daabase/factory/UserFactory」を元に作っているので、パスワードなのはそのファイルを参照
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(['name' => $this->user->name]);
+
+        $this->assertAuthenticatedAs($this->user);
+    }
+}
