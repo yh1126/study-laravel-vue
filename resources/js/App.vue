8fc891cb19708148fd9ec9851f927ce6f6baeea3
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-07-24 20:28:49 +0900

    headerとfooterの追加
    headerにnavigationを追加

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
+    <!-- header navigation -->
+    <header>
+      <Navbar />
+    </header>
     <main>
       <div class="container">
         <RouterView />
       </div>
     </main>
+    <Footer />
   </div>
 </template>
+
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar">
+    <RouterLink class="bavbar_brand" to="/">
+      Vuesplash
+    </RouterLink>
+    <div class="bavbar_menu">
+        <div class="navbar_item">
+            <button class="button">
+                <i class="icon ion-md-add"></i>
+                Submit a photo
+            </button>
+        </div>
+
+        <span class="navbar_item">
+          username
+        </span>
+        <div class="navbar_item">
+          <RouterLink class="button button--link" to="/login">
+            Login / Register
+          </RouterLink>
+        </div>
+    </div>
+  </nav>
+</template>
