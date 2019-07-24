d771d41fbbbfe6867ccc9a98e0b9ea41f2f50504
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-07-24 21:14:20 +0900

    register用のフォームとバインディングを行なった

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -26,7 +26,21 @@
       </form>
     </div>
 
-    <div class="panel" v-show="tab === 2">Register Form</div>
+    <div class="panel" v-show="tab === 2">
+      <form class="form" @submit.prevent="register">
+        <label for="username">Name</label>
+        <input type="text" class="form__item" id="username" v-model="registerForm.name">
+        <label for="email">Email</label>
+        <input type="text" class="form__item" id="email" v-model="registerForm.email">
+        <label for="password">Pssword</label>
+        <input type="password" class="form__item" id="password" v-model="registerForm.password">
+        <label for="password-confirmation">Password(confirm)</label>
+        <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+        <div class="form__butoon">
+          <button type="submit" class="button button--inverse">register</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -38,12 +52,21 @@ export default {
       loginForm: {
         email: '',
         password: ''
+      },
+      registerForm: {
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
       }
     }
   },
   methods: {
     login () {
       console.log(this.loginForm)
+    },
+    register () {
+      console.log(this.registerForm)
     }
   }
 }
