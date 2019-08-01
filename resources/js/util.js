095a8478153d17ba9bc4e44537bf327be3a399de
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-08-01 21:21:45 +0900

    CSRF対策の処理を実装

---
+++ resources/js/util.js
@@ -0,0 +1,24 @@
+/**
+ * クッキーの値を取得する
+ * @param {String} searchKey 検索するキー
+ * @returns {String} キーに対応する値
+ */
+export function getCookieValue(searchKey) {
+  if (typeof searchKey === 'undefined') {
+    return ''
+  }
+
+  let val = ''
+
+  // document.cookieは下記のような形式でcookieを参照できる
+  // name=12345;token=67890;key=abcde
+  document.cookie.split(';').forEach(cookie => {
+    console.log(cookie);
+    const [key, value] = cookie.split('=')
+    if (key === searchKey) {
+      return val = value
+    }
+  })
+
+  return val
+}
