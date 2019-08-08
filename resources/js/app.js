0e8237d0faabe5a2881514fa88db1d8433bb97e2
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-08-08 19:42:23 +0900

    不要行の削除

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,15 +19,6 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-
 import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
@@ -35,6 +26,9 @@ import router from './router'
 import App from './App.vue'
 import store from './store'
 
+
+
+// Vueインスタンスの生成
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
