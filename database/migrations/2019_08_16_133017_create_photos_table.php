60dbd2eaf963d5b5d4776f60b7c4ee5e8a745ad4
yh1126 <h_yamashita@corp.allabout.co.jp>
2019-08-16 22:47:07 +0900

    photoテーブル作成用のmigrationファイル作成

---
+++ database/migrations/2019_08_16_133017_create_photos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->string('id')->primary(); //idをstringにするのはなぜだ...
+            $table->unsignedInteger('user_id');
+            $table->string('filename');
+            $table->timestamps();
+
+            // userテーブルのidを外部キーとする
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
