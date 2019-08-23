d870c0dae49170f688d0f531d3cbf82babb754ce
yh1126 <cake_sk_yh@icloud.com>
2019-08-23 20:28:35 +0900

    ファイルを選択した時にプレビューを出すようにした

--- resources/js/components/PhotoForm.vue
+++ resources/js/components/PhotoForm.vue
@@ -2,7 +2,10 @@
   <div v-show="value" class="photo-form">
     <h2 class="title">Submit a Photo</h2>
     <form class="form">
-      <input class="form__item" type="file" />
+      <input class="form__item" type="file" @change="onFileChange">
+      <output class="form__output" v-if="preview">
+        <img :src="preview" alt="">
+      </output>
       <div class="form__button">
         <button type="submit" class="button button--inverse">submit</button>
       </div>
@@ -17,6 +20,50 @@ export default {
       //form表示の制御を行う
       type: Boolean,
       required: true
+    }
+  },
+  data () {
+    return {
+      preview: null
+    }
+  },
+  methods: {
+    // フォームでファイルが選択されたら実行される
+    onFileChange (event) {
+      // 何も選択されていなかったら処理中断
+      if (event.target.files.length === 0) {
+        this.reset()
+        return false
+      }
+
+      // ファイルが画像ではなかったら処理中断
+      if (! event.target.files[0].type.match('image.*')) {
+        this.reset()
+        return false;
+      }
+
+      // FileReaderクラスのインスタンを取得
+      const reader = new FileReader()
+
+      // ファイルを読み込み終わったタイミングで実行する処理
+      reader.onload = e => {
+        // previewに読み込み結果(データURL)を代入する
+        // previewに値が入ると<output>煮付けたv-ifがtrueと判定される
+        // また<ouput>内部の<img>のsrc属性はpreviewの値を参照しているので
+        // 結果として画像が表示される
+        this.preview = e.target.result
+        console.log(this.preview)
+      }
+
+      // ファイルを読み込む
+      // 読み込まれたファイルはデータURL形式で受け取れる(上記onload参照)
+      reader.readAsDataURL(event.target.files[0])
+    },
+    // 入力欄の値とプレビュー表示をクリアするメソッド
+    reset () {
+      this.prevoew = ''
+      // this.$elはコンポーネントそのもののDOM要素を指す
+      this.$el.querySelector('input[type="file"]').value = null
     }
   }
 };
