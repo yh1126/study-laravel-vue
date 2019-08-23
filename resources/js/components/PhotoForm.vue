6a7937380047398fc729e7b35ff6c909e0ba1070
yh1126 <cake_sk_yh@icloud.com>
2019-08-23 20:45:18 +0900

    送信ボタンをおしたらAPIにデータを送るようにした

--- resources/js/components/PhotoForm.vue
+++ resources/js/components/PhotoForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-show="value" class="photo-form">
     <h2 class="title">Submit a Photo</h2>
-    <form class="form">
+    <form class="form" @submit.prevent="submit">
       <input class="form__item" type="file" @change="onFileChange">
       <output class="form__output" v-if="preview">
         <img :src="preview" alt="">
@@ -52,18 +52,27 @@ export default {
         // また<ouput>内部の<img>のsrc属性はpreviewの値を参照しているので
         // 結果として画像が表示される
         this.preview = e.target.result
-        console.log(this.preview)
       }
 
       // ファイルを読み込む
       // 読み込まれたファイルはデータURL形式で受け取れる(上記onload参照)
       reader.readAsDataURL(event.target.files[0])
+      this.photo = event.target.files[0]
     },
     // 入力欄の値とプレビュー表示をクリアするメソッド
     reset () {
-      this.prevoew = ''
+      this.preview = ''
+      this.photo = null
       // this.$elはコンポーネントそのもののDOM要素を指す
       this.$el.querySelector('input[type="file"]').value = null
+    },
+    async submit () {
+      const formData = new FormData()
+      formData.append('photo', this.photo)
+      const response = await axios.post('/api/photos', formData)
+
+      this.reset()
+      this.$emit('input', false)
     }
   }
 };
