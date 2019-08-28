9d674b2a39b2ce27bbc8a65b366e374251887df8
yh1126 <cake_sk_yh@icloud.com>
2019-08-28 22:02:27 +0900

    写真モデル用のfactoryを作成

---
+++ database/factories/PhotoFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\Photo;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(Photo::class, function (Faker $faker) {
+    return [
+        'id' => str_random(12),
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'filename' => str_random(12) . '.jpg',
+        'created_at' => $faker->dateTime(),
+        'updated_at' => $faker->dateTIme(),
+    ];
+});
