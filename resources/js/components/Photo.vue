87116f2cd876921ac054dda72b02883dac47ea51
yh1126 <cake_sk_yh@icloud.com>
2019-08-31 04:30:42 +0900

    写真一覧用に写真用コンポーネントを追加

---
+++ resources/js/components/Photo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="photo">
+    <figure class="photo__wrapper">
+      <img
+        class="photo__image photo__image--portrait"
+        :src="item.url"
+        :alt="`Photo by ${item.owner.name}`"
+      >
+    </figure>
+    <RouterLink
+      class="photo__overlay"
+      :to="`/photos/${item.id}`"
+      :title="`View the photo by ${item.owner.name}`"
+    >
+      <div class="photo__controls">
+        <button
+          class="photo__action photo__action--like"
+          title="Like Photo"
+        >
+          <i class="icon icon-md-heart"></i>12
+        </button>
+        <!-- サーバに直接リクエストを送るためaタグを設定 -->
+        <a
+          class="photo__action"
+          title="Download photo"
+          @click.stop
+          :href="`/photos/${item.id}/download`"
+        >
+          <i class="icon icon-md-arrow-round-down"></i>
+        </a>
+      </div>
+      <div class="photo__username">
+        {{ item.owner.name }}
+      </div>
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- resources/js/pages/PhotoList.vue
+++ resources/js/pages/PhotoList.vue
@@ -1,3 +1,47 @@
 <template>
-  <h1>Photo List</h1>
+  <div class="photo-list">
+    <div class="grid">
+      <Photo
+        class="grid__item"
+        v-for="photo in photos"
+        :key="photo.id"
+        :item="photo"
+      />
+    </div>
+  </div>
 </template>
+
+<script>
+import { OK } from '../util'
+import Photo from '../components/Photo.vue'
+
+export default {
+  components: {
+    Photo
+  },
+  data () {
+    return {
+      photos: []
+    }
+  },
+  methods: {
+    async fetchPhotos () {
+      const response = await axios.get('/api/photos')
+
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+      }
+
+      this.photos = response.data.data
+    }
+  },
+  watch: {
+    $route: {
+      async handler() {
+        await this.fetchPhotos()
+      },
+      immediate: true //コンポーネントが生成されたタイミングでも実行するようにする
+    }
+  }
+}
+</script>
