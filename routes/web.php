464ca78637a24e3af99aaa41f5c9345855b87a45
yh1126 <cake_sk_yh@icloud.com>
2019-09-04 22:09:59 +0900

    downloadメソッドで指定する画像のパスが間違っていたので修正
    routesも定義する場所が間違っていたので修正

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,11 @@
 |
 */
 
+Route::get('/photos/{photo}/download', 'PhotoController@download');
+
 // APIのURL以外のリクエストに対してはindexテンプレートを返す
 // 画面遷移はフロントエンドのVueRouterが制御する
 Route::get('/{any?}', function () {
     return view('index');
 })->where('any', '.+');
+
