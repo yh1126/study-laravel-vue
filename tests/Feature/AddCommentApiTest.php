dbd19562d8f02cdf0895ac80e7ebecc8c2390428
yh1126 <cake_sk_yh@icloud.com>
2019-09-17 22:26:31 +0900

    コメント追加処理のテストを作成

---
+++ tests/Feature/AddCommentApiTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Photo;
+use App\Models\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AddCommentApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function should_コメントを追加できる()
+    {
+        factory(Photo::class)->create();
+        $photo = Photo::first();
+
+        $content = 'sample content';
+
+        $response = $this->actingAs($this->user)
+            ->json('POST', route('photo.coment', [
+                'photo' => $photo->id,
+            ]), compact('content'));
+        $comments = $pthoto->comments()->get();
+
+        // 記事に従って書いてるけど、テスト三分割した方が良さそう
+        $response->assertStatus(201)
+        // JSONフォーマットが期待通りであること
+        ->assertJsonFragment([
+            "author" => [
+                "name" => $this->user->name,
+            ],
+            "content" => $content,
+        ]);
+
+        // DBにコメントが一見登録されていること
+        $this->assertEquals(1, $comments->count());
+        // 内部がAPIでリクエストしたものであること
+        $this->assertEquals($content, $comments[0]->content);
+    }
+}
