c5578eb4b6fa67a456df0312c2a8821512e3b7d4
yh1126 <cake_sk_yh@icloud.com>
2019-09-17 22:44:09 +0900

    コメント用のモデルを作成した

---
+++ app/Models/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Comment extends Model
+{
+    /** JSONに含める属性 */
+    protected $visible = [
+        'author', 'content',
+    ];
+
+    /**
+     * リレーションの定義 -useテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id',  'id', 'user');
+    }
+}
