6e2d5b6a7955835512d766be12649a7abf8b102c
yh1126 <cake_sk_yh@icloud.com>
2019-09-20 20:49:28 +0900

    不要な空白とuse文を削除

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 
 class Comment extends Model
 {
@@ -18,6 +17,6 @@ class Comment extends Model
      */
     public function author()
     {
-        return $this->belongsTo(User::class, 'user_id',  'id', 'user');
+        return $this->belongsTo(User::class, 'user_id', 'id', 'user');
     }
 }
